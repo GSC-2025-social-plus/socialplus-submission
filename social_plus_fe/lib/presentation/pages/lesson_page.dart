7317c67a24f45f999344b76c403b711ea9714d8e
TaesungJung <wjdxotjdfgo@gmail.com>
2025-05-15 20:21:13 +0900

    chat_bot_V7_image3

--- social_plus_fe/lib/presentation/pages/lesson_page.dart
+++ social_plus_fe/lib/presentation/pages/lesson_page.dart
@@ -100,7 +100,7 @@ class _LessonMissionsScreenState extends State<LessonMissionsScreen> {
                         );
                       },
                       icon: Image.asset(
-                        'assets/images/rightArrowCircle.png',
+                        'assets/images/leftArrowCircle.png',
                         width: 24,
                         height: 24,
                         color: Colors.white,
